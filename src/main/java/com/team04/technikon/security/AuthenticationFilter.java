9e145c0c9674c86ea4743b3e327f33e5e43d90d4
TasosBrb <tasosbarbakas@gmail.com>
2023-01-19 01:00:25 +0200

    Authentication Filter added

---
+++ src/main/java/com/team04/technikon/security/AuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.team04.technikon.security;
+
+
+import com.team04.technikon.repository.PropertyOwnerRepository;
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ResourceInfo;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+@Provider
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+    private static final String AUTHORIZATION_PROPERTY = "Authorization";
+    private static final String AUTHENTICATION_SCHEME = "Basic";
+    @Inject
+    private PropertyOwnerRepository ownerRepository;
+    @Context
+    private ResourceInfo resourceInfo;
+
+    @Override
+    public void filter(ContainerRequestContext crc) throws IOException {
+        Method method = resourceInfo.getResourceMethod();
+        if (method.isAnnotationPresent(PermitAll.class)) {
+            return;
+        }
+        if (method.isAnnotationPresent(DenyAll.class)) {
+            crc.abortWith(Response.status(Response.Status.FORBIDDEN).entity("Access blocked for all users !!").build());
+            return;
+        }
+        final MultivaluedMap<String, String> headers = crc.getHeaders();
+        final List<String> authorization = headers.get(AUTHORIZATION_PROPERTY);
+        if (authorization == null || authorization.isEmpty()) {
+            crc.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("You cannot access this resource").build());
+            return;
+        }
+        final String encodedUserPassword = authorization.get(0).replaceFirst(AUTHENTICATION_SCHEME + " ", "");
+        String usernameAndPassword = new String(Base64.getDecoder().decode(encodedUserPassword.getBytes()));
+        final StringTokenizer tokenizer = new StringTokenizer(usernameAndPassword, ":");
+        final String username = tokenizer.nextToken();
+        final String password = tokenizer.nextToken();
+
+        if (method.isAnnotationPresent(RolesAllowed.class)) {
+            RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
+            Set<String> rolesSetForTheResource = new HashSet<>(Arrays.asList(rolesAnnotation.value()));
+
+            //Is user valid?
+            if (!isUserAllowed(username, password, rolesSetForTheResource)) {
+                crc.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("You cannot access this resource").build());
+            }
+        }
+
+    }
+
+    private boolean isUserAllowed(final String username, final String password, final Set<String> rolesSetExpected) {
+        return rolesSetExpected.contains(ownerRepository.checkRole(username, password));
+    }
+}
---
+++ src/main/java/com/team04/technikon/security/CorsFilter.java
@@ -0,0 +1,26 @@
+package com.team04.technikon.security;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author iracl
+ */
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+  public void filter(ContainerRequestContext crc, ContainerResponseContext crc1) throws IOException {
+    crc1.getHeaders().add("Access-Control-Allow-Origin", "*");
+    crc1.getHeaders().add("Access-Control-Allow-Credentials", "true");
+    crc1.getHeaders().add("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+    crc1.getHeaders().add("Access-Control-Allow-Headers", "username, password, X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+    crc1.getHeaders().add("Access-Control-Max-Age", "1209600");
+    crc1.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, crossdomain");
+    }
+}
